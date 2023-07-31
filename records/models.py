61a0d2113c7235e606ede60adf125f485e6f41e7
prubayita <prubayita@gmail.com>
2023-07-31 10:21:19 +0200

    int Field

--- records/models.py
+++ records/models.py
@@ -32,7 +32,7 @@ class Visitor(models.Model):
 
 class Card(models.Model):
     name= models.CharField(max_length=50)
-    number = models.CharField(max_length=50)
+    number = models.IntegerField(max_length=50)
 
     def __str__(self):
         return self.name
